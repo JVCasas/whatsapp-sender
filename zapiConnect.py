2023c2415a260d055e880c38edbc68e545420b0e
Juan <juanvc2010@gmail.com>
2026-06-17 09:01:50 -0300

    Maintainability adjustments and addition of error handling

--- zapiConnect.py
+++ zapiConnect.py
@@ -1,10 +1,24 @@
+import os
+from dotenv import load_dotenv
 import requests
 
-def sendMessage(zapiId:str, zapiToken:str, phone:str, message:str):
-    url = f'https://api.z-api.io/instances/{zapiId}/token/{zapiToken}/send-text'
+load_dotenv()
+
+zId:str = os.getenv('ZAPI_ID')
+zToken:str = os.getenv('ZAPI_TOKEN')
+
+def sendMessage(phone:str, message:str):
+    url = f'https://api.z-api.io/instances/{zId}/token/{zToken}/send-text'
     payload = {"phone": phone, "message": message}
     header = {"Client-Token": url, "Content-Type": "application/json"}
 
-    response = requests.post(url, json = payload, headers = header)
+    try:
+        response = requests.post(url, json = payload, headers = header)
+        
+        response.raise_for_status()
 
-    print(response.text)
+        print(response.json())
+    
+    except requests.exceptions.RequestException as e:
+        print('Erro ao enviar mensagem!')
+        print(e)
