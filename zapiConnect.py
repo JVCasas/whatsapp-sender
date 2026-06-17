038aa592031e050b0688cc769db34f92230936f9
Juan <juanvc2010@gmail.com>
2026-06-16 21:23:52 -0300

    Connection Test

--- zapiConnect.py
+++ zapiConnect.py
@@ -1,5 +1,17 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
+zapi = os.getenv('ZAPI')
+
+def sendMessage():
+    payload = {"phone": "5521988670501", "message": 'Olá mundo'}
+    header = {"Client-Token": zapi, "Content-Type": "application/json"}
+
+    response = requests.post(zapi, json = payload, headers = header)
+
+    print(response.text)
+
+sendMessage()
